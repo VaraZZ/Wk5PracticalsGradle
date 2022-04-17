a76cd57aefaf02f2c9793ba4ba6e77727924e55a
VaraZZ <96443506+VaraZZ@users.noreply.github.com>
2022-04-17 06:04:24 +0000

    Deploying to gh-pages from @ VaraZZ/Wk5PracticalsGradle@0347a300e8109fd6e069ec081244f650db30d99e 🚀

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"Wk5PracticalsGradle","c":"App","l":"App()","url":"%3Cinit%3E()"},{"p":"Wk5PracticalsGradle","c":"App","l":"getGreetings()"},{"p":"Wk5PracticalsGradle","c":"App","l":"main(String[])","url":"main(java.lang.String[])"}]
